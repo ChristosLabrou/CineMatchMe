b2f33e5d93b71dc2f6720e8b6f7dd3d606295e00
sphantom99 <georgeal1999@gmail.com>
2021-06-07 20:54:25 +0300

    made SnackBars to inform user of result instead

--- app/src/main/java/com/example/myapplication/SearchTitle.java
+++ app/src/main/java/com/example/myapplication/SearchTitle.java
@@ -7,11 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.PopupWindow;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class SearchTitle extends AppCompatActivity {
 
     public void checkIfMovieExists(View view){
-        Intent intent = new Intent(this,movieExistsPopup.class);
-        startActivity(intent);
+        //make call to DB and find if it exists
+        boolean exists = new Boolean(false);
+        if(exists){
+            Snackbar.make(findViewById(R.id.myCoordinatorLayout), "This Movie Already Exists!",
+                    Snackbar.LENGTH_SHORT)
+                    .show();
+        } else {
+            Intent intent = new Intent(this,addMovieForm.class);
+            startActivity(intent);
+        }
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
